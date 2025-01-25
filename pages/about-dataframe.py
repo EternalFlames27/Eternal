c5acc8e5a03f93193bd5135f9d8a584d5291b869
EternalFlames27 <Shinjiolivertjeng13@gmail.com>
2025-01-25 09:31:11 +0700

    Add files via upload

---
+++ pages/about-dataframe.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = pd.read_excel('source.xlsx')
+
+criteria1 = data['category'] == 'food'
+criteria2 = data['store'] == 5
+criteria3 = data['price'] >= 12000
+criteria4 = (data['price'] >= 12000) & (data['price'] <= 20000)
+criteria5 = (criteria4) & (criteria2)
+
+data[criteria5]
+
+
+print(data[criteria5].sort_values('price',ascending=True))
