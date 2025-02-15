77853cba3b898e28e6963e997fdb788decc7b500
EternalFlames27 <Shinjiolivertjeng13@gmail.com>
2025-02-15 09:37:02 +0700

    Update about-dataframe.py

--- pages/about-dataframe.py
+++ pages/about-dataframe.py
@@ -20,4 +20,9 @@ join_criteria =  (criteria1) & (criteria2) & (criteria3)
 
 with st.container(border=True):
   data = data[join_criteria]
+
+  product_picture = data.iloc[0]['picture']
+  st.image(Product_picture)
+
+  
   st.dataframe(data,use_container_width=True)
