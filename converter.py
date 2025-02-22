1ce0159a84bd23de5c5d70bec99d27675f3984d9
EternalFlames27 <Shinjiolivertjeng13@gmail.com>
2025-02-22 10:20:15 +0700

    Update converter.py

--- converter.py
+++ converter.py
@@ -7,6 +7,21 @@ conversion_factors = {
   'weight':None,
   'time':None,
 }
-
+col1,col2,col3,col4,col5 = st.columns(5)
+with col1:
 category_list = list(conversion_factor.keys())
 category = st.radio("select category",options=category_list)
+
+with col2:
+  st.write("Input")
+
+with col3:
+base_unit_list = list(conversion_factors[category].keys())
+base_unit = st.radio("from:",base_unit_list)
+
+with col4:
+target_unit_list = list(conversion_factors[category].keys())
+target_unit = st.radio("To:",target_unit)
+
+with col5:
+  st.write("output")
