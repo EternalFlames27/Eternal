4b60373e5b63a2beefb221d62717c62a52267142
EternalFlames27 <Shinjiolivertjeng13@gmail.com>
2025-03-01 10:01:31 +0700

    Update converter.py

--- pages/converter.py
+++ pages/converter.py
@@ -4,8 +4,23 @@ conversion_factors = {
   'distance':{'mm':1,
               'cm':0.1,
               'm':0.01},
+
+  'calories':{'fries':319,
+              'donut':190,
+              'burger:290,
+              'ice cream':207,
+              'apple':52.1,
+              'banana':52.1,
+              'cabbage':24.6,
+              'coke':139,
+              'milk'42.3},
+              
   'weight':None,
   'time':None,
+  
+              
+              
+              
 }
 col1,col2,col3,col4,col5 = st.columns(5)
 with col1:
