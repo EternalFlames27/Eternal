1a721dca8829992322651422cba1bb2458107fd5
EternalFlames27 <Shinjiolivertjeng13@gmail.com>
2025-03-08 10:15:39 +0700

    Update about-dataframe.py

--- pages/about-dataframe.py
+++ pages/about-dataframe.py
@@ -10,23 +10,35 @@ selected_category = st.multiselect("select category",options=unique_category)
 selected_store = st.multiselect("select store",options=unique_storename)
 price_point = st.slider("Price",min_value=minimum_price,max_value=maximum_price,value=maximum_price)
 
+
 criteria1 = data['category'].isin(selected_category) 
 criteria2 = data['store'].isin(selected_store)
 criteria3 = data['price'] <= price_point
 
 join_criteria =  (criteria1) & (criteria2) & (criteria3)
 
+data = data[join_criteria]
+data_count = len(data)
 
+columns = st.columns(ncolumns)
 
-with st.container(border=True):
-  data = data[join_criteria]
-  data_count = len(data)
-  
-  for i in range(data_count):
-    product_picture = data.iloc[i]['picture']
-    st.image(product_picture,width = 240)
-    if st.button("buy now!",key=str(i)):
-      st.write("Thank you for buying our product! It will arrive shortly.")
+for i in range(data_count):
+  for c in range(ncolumns):
+    if i%ncolumns == c:
+      col = columns[c]
+      with col:
+        product_picture = data.iloc[i]['picture']
+        st.image(product_picture,width = 240)
+
+        btnc1,btnc2 = st.columns(2)
+    
+        with st.container():
+          with btnc:
+            if st.button("buy now!",key=str(i)):
+              st.write("Thank you for buying this product! It will arrive shortly.")
+          with btnc2:
+            if st.button("Add to cart",key=str(i)+"a"):
+              st.write("Added to cart! dont forget about it!")
 
   
   st.dataframe(data,use_container_width=True)
